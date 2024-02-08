467588243a8575b91af81587f82ac96ddc9df54f
Tamar Nehemia <tamar5388@gmail.com>
2024-02-08 17:12:09 +0200

    Add files via upload

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"a6813fb3-607e-4818-b6c5-c134b8590dcd"}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
